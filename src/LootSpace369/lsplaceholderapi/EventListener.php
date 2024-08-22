c1dc37698d698e1410ec35da64e16f893e5cd275
Vasync <tbao12901@gmail.com>
2024-08-22 23:49:18 +0700

    Update EventListener.php

--- src/LootSpace369/lsplaceholderapi/EventListener.php
+++ src/LootSpace369/lsplaceholderapi/EventListener.php
@@ -16,6 +16,20 @@ use pocketmine\network\mcpe\protocol\TextPacket;
 use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
 use pocketmine\lang\Translatable;
 
+const THIN_TAG = "\xc2\xa7";
+
+function toThin(array|string $arr) : array|string {
+        $r = [];
+        if (is_string($arr)) return preg_replace("/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i", "%$1", $arr) . THIN_TAG;
+        
+        foreach ($arr as $str) {
+            if ($str === "") {$r[] = "";}else{
+            $r[] = preg_replace("/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i", "%$1", $str) . THIN_TAG;
+            }
+        }
+        return $r;
+}
+
 class EventListener implements Listener {
     
     /**
@@ -24,21 +38,21 @@ class EventListener implements Listener {
     public function onDataPacketSend(DataPacketSendEvent $event): void {
         foreach ($event->getTargets() as $target) {
             foreach ($event->getPackets() as $packet) {
-                if ($packet instanceof TextPacket) $packet->message = PlaceHolderAPI::replace($packet->message);
+                if ($packet instanceof TextPacket) $packet->message = toThin(PlaceHolderAPI::replace($packet->message));
                 if ($packet instanceof ModalFormRequestPacket) {
                     $formData = json_decode($packet->formData, true);
                     
-                    if (isset($formData["title"])) $formData["title"] = PlaceHolderAPI::replace($formData["title"]);
+                    if (isset($formData["title"])) $formData["title"] = toThin(PlaceHolderAPI::replace($formData["title"]));
                     if (!is_array($formData["content"])) {
-                        if (isset($formData["content"])) $formData["content"] = PlaceHolderAPI::replace($formData["content"]);
+                        if (isset($formData["content"])) $formData["content"] = toThin(PlaceHolderAPI::replace($formData["content"]));
                     }else{
-                        foreach ($formData["content"] as $content => $value) if (isset($formData["content"][$content]["text"])) $formData["content"][$content]["text"] = PlaceHolderAPI::replace($formData["content"][$content]["text"]);
+                        foreach ($formData["content"] as $content => $value) if (isset($formData["content"][$content]["text"])) $formData["content"][$content]["text"] = toThin(PlaceHolderAPI::replace($formData["content"][$content]["text"]));
                     }
                     if (isset($formData["buttons"])) {
-                        foreach ($formData["buttons"] as $button => $value) if (isset($formData["buttons"][$button]["text"])) $formData["buttons"][$button]["text"] = PlaceHolderAPI::replace($formData["buttons"][$button]["text"]);
+                        foreach ($formData["buttons"] as $button => $value) if (isset($formData["buttons"][$button]["text"])) $formData["buttons"][$button]["text"] = toThin(PlaceHolderAPI::replace($formData["buttons"][$button]["text"]));
                     }
-                    if (isset($formData["button1"])) $formData["button1"] = PlaceHolderAPI::replace($formData["button1"]);;
-                    if (isset($formData["button2"])) $formData["button2"] = PlaceHolderAPI::replace($formData["button2"]);
+                    if (isset($formData["button1"])) $formData["button1"] = toThin(PlaceHolderAPI::replace($formData["button1"]));
+                    if (isset($formData["button2"])) $formData["button2"] = toThin(PlaceHolderAPI::replace($formData["button2"]));
                     $packet->formData = json_encode($formData);
                 }
             }
